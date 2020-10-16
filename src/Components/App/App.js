94ce3bf0b1c3332f47bafb722620f66b321ffb1a
MartaLourido <martalouri@gmail.com>
2020-10-16 16:41:30 +0200

    jammming project

---
+++ src/Components/App/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+import './App.css';
+// import SearchBar from '../SearchBar/SearchBar';
+// import SearchResults from '../SearchResults/SearchResults';
+// import Playlist from '../Playlist/Playlist';
+
+export default class App extends Component {
+    render() {
+        return (
+            <div>
+            <h1>Ja<span className="highlight">mmm</span>ing</h1>
+            <div className="App">
+            
+              <div className="App-playlist">
+               
+              </div>
+            </div>
+          </div>
+        )
+    }
+}
+
+
